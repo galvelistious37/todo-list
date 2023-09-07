bd9cff9728a6ae76ff5ef920841ef3c1a72c1c9b
johnny <john.robertson88@yahoo.com>
2023-09-07 00:14:02 -0700

    Add functionality to switch between dail and work tasks while holding on to tast status checked between both pages

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ app.get("/", function(req, res){
 })
 
 let dailyTasks = []
-let count = 0
-
+let dailyCount = 0
+let workTasks = []
+let workCount = 0
 
 app.post("/goToDailyTasks", function(req, res){
     console.log("size(): " + dailyTasks.length)
@@ -26,7 +27,7 @@ app.post("/goToDailyTasks", function(req, res){
 
 app.post("/dailyTasks", function(req, res){
     let task = req.body["task"]
-    let id = count
+    let id = dailyCount
     let formStart = '<form action="/updateDailyTask" method="POST">'
     let type = "<input type='checkbox' id='" + id 
         + "' name='task' value='" + id + "' class='checks' onclick='this.form.submit()'></input>"
@@ -36,7 +37,7 @@ app.post("/dailyTasks", function(req, res){
 
     dailyTasks.push({"id": id, "formStart": formStart, "type": type, "label": label, "formEnd": formEnd})
     res.render("dailyTasks.ejs", {tasks: dailyTasks})
-    count++
+    dailyCount++
 })
 
 app.post("/updateDailyTask", function(req, res){
@@ -49,6 +50,43 @@ app.post("/updateDailyTask", function(req, res){
     let newString = tempString.replace("class=''", "class='strikethrough'")
     dailyTasks[id].label = newString
     res.render("dailyTasks.ejs", {tasks: dailyTasks})
+})
+
+
+app.post("/goToWorkTasks", function(req, res){
+    console.log("size(): " + workTasks.length)
+    if(workTasks.length > 0){
+        res.render("workTasks.ejs", {tasks: workTasks})
+    } else {
+        res.render("workTasks.ejs")
+    }
+})
+
+app.post("/workTasks", function(req, res){
+    let task = req.body["task"]
+    let id = workCount
+    let formStart = '<form action="/updateWorkTask" method="POST">'
+    let type = "<input type='checkbox' id='" + id 
+        + "' name='task' value='" + id + "' class='checks' onclick='this.form.submit()'></input>"
+
+    let label = "<label for='task'><span id='span" + id + "' class=''>" + task + "</span></label><br>"
+    let formEnd = '</form><br>'
+
+    workTasks.push({"id": id, "formStart": formStart, "type": type, "label": label, "formEnd": formEnd})
+    res.render("workTasks.ejs", {tasks: workTasks})
+    workCount++
+})
+
+app.post("/updateWorkTask", function(req, res){
+    let id = req.body["task"]
+    workTasks[id].type = "<input type='checkbox' id='" + id 
+    + "' name='task" + id 
+    + "' value='" + id + "' class='checks' checked='yes' disabled='disabled'></input>"
+
+    let tempString = workTasks[id].label
+    let newString = tempString.replace("class=''", "class='strikethrough'")
+    workTasks[id].label = newString
+    res.render("workTasks.ejs", {tasks: workTasks})
 })
 
 app.listen(port, () =>{
